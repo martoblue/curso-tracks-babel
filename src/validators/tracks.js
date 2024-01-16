e5b2f88dc3bd25984f196cac07a3c73e4aa80084
Martorelli <luis.martorelli@bm-maritima.cl>
2024-01-16 17:31:48 -0300

    Se realizan cambios de CRUD sin validadores

--- src/validators/tracks.js
+++ src/validators/tracks.js
@@ -13,7 +13,6 @@ const validatorCreateItem = [
   check('mediaId').exists().notEmpty(),
   (req, res, next) => {
     try {
-
       validationResults(req, res, next);
       return next(); // Si no hay errores, continua con la siguiente función de middleware
     } catch (error) {
@@ -23,18 +22,18 @@ const validatorCreateItem = [
   },
 ];
 
-const validatorUpdateItem = [
-  check('id').exists().notEmpty().withMessage('El id es obligatorio'),
-  check('name').exists().notEmpty().withMessage('El nombre es obligatorio'),
-  check('album').exists().notEmpty().withMessage('El album es obligatorio'),
-  check('cover').exists().notEmpty().withMessage('El cover es obligatorio'),
-  check('artist_name').exists().notEmpty().withMessage('El artista es obligatorio'),
-  check('artist_nickname').exists().notEmpty().withMessage('El artista es obligatorio'),
-  check('artist_nationality').exists().notEmpty().withMessage('El artista es obligatorio'),
-  check('duration_start').exists().notEmpty().withMessage('La duración es obligatoria'),
-  check('duration_end').exists().notEmpty().withMessage('La duración es obligatoria'),
-  check('mediaId').exists().notEmpty().withMessage('El mediaId es obligatorio'),
-];
+// const validatorUpdateItem = [
+//   check('id').exists().notEmpty().withMessage('El id es obligatorio'),
+//   check('name').exists().notEmpty().withMessage('El nombre es obligatorio'),
+//   check('album').exists().notEmpty().withMessage('El album es obligatorio'),
+//   check('cover').exists().notEmpty().withMessage('El cover es obligatorio'),
+//   check('artist_name').exists().notEmpty().withMessage('El artista es obligatorio'),
+//   check('artist_nickname').exists().notEmpty().withMessage('El artista es obligatorio'),
+//   check('artist_nationality').exists().notEmpty().withMessage('El artista es obligatorio'),
+//   check('duration_start').exists().notEmpty().withMessage('La duración es obligatoria'),
+//   check('duration_end').exists().notEmpty().withMessage('La duración es obligatoria'),
+//   check('mediaId').exists().notEmpty().withMessage('El mediaId es obligatorio'),
+// ];
 
 const validatorGetItem = [
   check('id').exists().notEmpty(),
@@ -43,4 +42,4 @@ const validatorGetItem = [
   },
 ];
 
-export { validatorCreateItem, validatorUpdateItem, validatorGetItem };
+export { validatorCreateItem, validatorGetItem };
